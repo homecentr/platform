712bb4c1e0de9b26a4b17dac5b18786e060c975f
LH <me@lholota.com>
2022-10-27 15:49:54 +0200

    Kickstart fix 3

--- scripts/kickstart-pve.sh
+++ scripts/kickstart-pve.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get install sudo
 
 read -p "Enter non-root admin user name: " USERNAME </dev/tty
+# read -p "Enter non-root admin user's full name: " USER_FULLNAME </dev/tty
 
-adduser $USERNAME </dev/tty
+adduser $USERNAME --disabled-password --gecos ''
 usermod -aG sudo $USERNAME
