dae4036754a116c0c293f89a5d1ed2ca51671065
LH <me@lholota.com>
2022-10-27 15:45:27 +0200

    Kickstart fix

--- scripts/kickstart-pve.sh
+++ scripts/kickstart-pve.sh
@@ -3,8 +3,7 @@
 apt-get update
 apt-get install sudo
 
-echo "Enter non-root admin user name:"
-read USERNAME
+read -p "Enter non-root admin user name:" USERNAME </dev/tty
 
 adduser $USERNAME
 usermod -aG sudo $USERNAME
