5d24fea300402cf35bc38d68457bf1ee22421cc3
Lukas Holota <me@lholota.com>
2024-02-14 10:00:55 +0100

    Removed unnecesarry code

--- tools/clearkeys.sh
+++ tools/clearkeys.sh
@@ -3,7 +3,3 @@
 ssh-keygen -f ~/.ssh/known_hosts -R 10.1.8.11
 ssh-keygen -f ~/.ssh/known_hosts -R 10.1.8.12
 ssh-keygen -f ~/.ssh/known_hosts -R 10.1.8.13
-
-ssh-keygen -f ~/.ssh/known_hosts -R 10.1.8.21
-ssh-keygen -f ~/.ssh/known_hosts -R 10.1.8.22
-ssh-keygen -f ~/.ssh/known_hosts -R 10.1.8.23
