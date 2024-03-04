0c5a343b899358b5de9af1def3b53466090efa97
Lukas Holota <me@lholota.com>
2024-03-04 21:58:44 +0100

    Checkpoint

--- tools/install.sh
+++ tools/install.sh
@@ -3,4 +3,6 @@
 # yarn install is executed automatically
 
 ansible-galaxy install -r ./requirements.yaml --force
-ansible-playbook ./playbooks/_local.yaml
+ansible-playbook ./playbooks/local/setup.yaml
+ansible-playbook -i ./environments/lab/ ./playbooks/local/ssh.yaml
+ansible-playbook -i ./environments/prod/ ./playbooks/local/ssh.yaml
